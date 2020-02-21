14ff6e2e5cb59a568b08c803c09c6814a28e6b47
Marian Ramirez <mramirez83@ucol.mx>
2020-02-21 16:45:46 -0600

    Perfil de paciente con todo el módulo de fecha

--- paciente.js
+++ paciente.js
@@ -1,26 +1,24 @@
+import Fecha from "./fecha.js";
+
 class Paciente {
     /**
      * 
      * @param {string} nombre 
-     * @param {number} fechaNacimiento 
+     * @param {Fecha} fechaNacimiento 
      * @param {number} telefono 
      */
     constructor(nombre, fechaNacimiento, telefono) {
         this.nombre = nombre;
-        this.fechaNacimiento = new Date(año, mes-1, dia);
+        this.fechaNacimiento = fechaNacimiento;
         this.telefono = telefono;
     }
 
-    getFechaNacimiento() {
-        return(`${this.fechaNacimiento.getDate()}/${this.fechaNacimiento.getMonth()+1}/${this.fechaNacimiento.getFullYear()}`);
-    }
-
     getPerfil() {
-        return (`Nombre: ${this.nombre}
-        Fecha de nacimiento: ${this.getFechaNacimiento.getFechaNacimiento}
+        return (`        Nombre: ${this.nombre}
+        Fecha de nacimiento: ${this.fechaNacimiento.getFechaa()}
         Tel.: ${this.telefono}`);
     }
 }
 
-let p1 = new Paciente("Marian Ramírez", new fechaNacimiento(2000, 11, 27), 3131590469);
+let p1 = new Paciente("Marian Ramírez", new Fecha(27, 11, 2000), 3131590469);
 console.log(p1.getPerfil());
